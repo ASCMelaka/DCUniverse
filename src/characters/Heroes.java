7830b7ff68ab05a2a5fb2deafbdfd1e9591ae998
Yk <pohyungkien@gmail.com>
2015-04-07 10:31:18 +0800

    Update Heroes.java

--- src/characters/Heroes.java
+++ src/characters/Heroes.java
@@ -4,7 +4,7 @@ package characters;
  * Created by navenprasad25 on 03/04/2015.
  */
  //Edited by Yk on 7/4/15
-public abstract class Heroes extends Characters{
+public class Heroes extends Characters{
     Heroes(String name){
         super(name);
     }
