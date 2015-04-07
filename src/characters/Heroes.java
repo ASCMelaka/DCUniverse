52099fb57d44246718768b3a1eb41a0e98097752
Yk <pohyungkien@gmail.com>
2015-04-07 10:19:03 +0800

    Update Heroes.java
    Delete unnecessary code redundancy in this class.

--- src/characters/Heroes.java
+++ src/characters/Heroes.java
@@ -3,28 +3,9 @@ package characters;
 /**
  * Created by navenprasad25 on 03/04/2015.
  */
+ //Edited by Yk on 7/4/15
 public abstract class Heroes extends Characters{
-    protected int life;
-
     Heroes(String name){
         super(name);
     }
-
-    public int getLife(){
-        return  life;
-    }
-
-    public boolean checkAlive() {
-        if(life <= 0)
-            alive = false;
-        return alive;
-    }
-
-    public void takeDamage(int damage){
-        life = life - damage;
-    }
-
-
-    public abstract int fight();
-
 }
