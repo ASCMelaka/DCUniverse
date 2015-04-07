ac7c3b57f2bc3462ad833d060971af027ebe0bba
Yk <pohyungkien@gmail.com>
2015-04-07 10:17:19 +0800

    Update Characters.java
    A better way of writing the code to reduce code redundancy

--- src/characters/Characters.java
+++ src/characters/Characters.java
@@ -3,7 +3,10 @@ package characters;
 /**
  * Created by navenprasad25 on 03/04/2015.
  */
+ 
+ //Edited by Yk on 7/4/15
 public abstract class Characters {
+    protected int life;
     private String name;
     protected boolean alive;
 
@@ -24,9 +27,20 @@ public abstract class Characters {
     public boolean isAlive(){
         return alive;
     }
+    public int getLife(){
+        return  life;
+    }
 
+    public boolean checkAlive() {
+        if(life <= 0)
+            alive = false;
+        return alive;
+    }
 
-
+    public void takeDamage(int damage){
+        life = life - damage;
+    }
+    public abstract int fight();
 }
 
 
